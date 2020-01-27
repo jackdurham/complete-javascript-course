936dd36568f183845d43db4d72cdf92f1e0678b4
Jack Durham <jackdurham11@gmail.com>
2020-01-27 15:56:10 +0000

    more javascript

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -582,9 +582,40 @@ if (john.calcBMI() > mark.calcBMI()) {
 * Loops and iteration
 */
 
-
+/*
 // for loop
-
 for (var i = 0; i <= 10; i++) {
   console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+for (var i = 0; i < john.length; i++) {
+  console.log(john[i]);
+}
+
+var i = 0;
+while (i < john.length) {
+  console.log(john[i]);
+  i++;
+}
+*/
+
+/*
+// continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') break;
+  console.log(john[i]);
+}
+*/
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = john.length - 1; i >= 0; i--) {
+  console.log(john[i]);
 }
