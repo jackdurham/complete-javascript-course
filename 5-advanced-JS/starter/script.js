32640f471c844839024f6e0279974791c446693a
Jack Durham <jackdurham11@gmail.com>
2020-02-03 12:24:15 +0000

    callback functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,5 @@
 // Function constuctor
-
+/*
 var john = {
   name: 'John',
   yearOfBirth: 1990,
@@ -10,11 +10,105 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this. job = job;
-  this.calculateAge = function() {
+};
+
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'desinger');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+*/
+
+
+//Object.create
+/*
+var personProto = {
+  calculateAge: function() {
     console.log(2016 - this.yearOfBirth);
   }
 };
 
-var john = new Person('John', 1990, 'teacher');
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-john.calculateAge();
+var jane = Object.create(personProto,
+  {
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+  });
+*/
+
+
+//Primatives vs objects
+/*
+//Primatives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Libson'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "San Francisco";
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+
+//Lecture: Passing functions as objects
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
