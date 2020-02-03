de9bb62fbd1d56318a36d9b71befec5ffedde514
Jack Durham <jackdurham11@gmail.com>
2020-02-03 09:09:24 +0000

    functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -10,6 +10,11 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this. job = job;
+  this.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+  }
 };
 
 var john = new Person('John', 1990, 'teacher');
+
+john.calculateAge();
