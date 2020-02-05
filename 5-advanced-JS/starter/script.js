a21eba86c050fee81281109ef5257085ed69a5d0
Jack Durham <jackdurham11@gmail.com>
2020-02-05 16:19:35 +0000

    commit

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -110,5 +110,21 @@ function calculateAge(el) {
   return 2016 - el;
 }
 
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  } else {
+    return -1;
+  }
+}
+
 var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
+console.log(fullAges);
+console.log(rates);
